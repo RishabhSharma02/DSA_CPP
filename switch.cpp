0f6811e4fa4054746e9ef586c7ac13e2383e74ca
Rishabh Kumar Sharma <rishabhsharma28302@gmail.com>
2022-08-11 21:08:31 +0530

    progs

--- switch.cpp
+++ switch.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 int main(){
     int age;
-    
+    cout<<"Enter age: "<<endl;
+    cin>>age;
+    switch(age){
+        case 16:
+        cout<<"Your age is 16.\n"<<endl;
+        break;
+    }
 }
