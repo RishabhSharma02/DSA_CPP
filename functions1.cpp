3dd989e0207512bedbb13e72daa1ddc06a8646c0
Rishabh Kumar Sharma <rishabhsharma28302@gmail.com>
2022-08-13 12:05:54 +0530

    progs

---
+++ functions1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+}
+class Employee(){
+    public:
+    string name;
+    int id;
+    Employee har;
+    har.name = "Something";
+har.id = 1;
+cout<<"the name of first employee is "<<har.name<<endl;
+
+}
+
