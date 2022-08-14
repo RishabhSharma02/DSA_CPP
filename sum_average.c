5b014717c861b807cad06fa592c879a540fb1428
Rishabh Kumar Sharma <rishabhsharma28302@gmail.com>
2022-08-14 18:39:15 +0530

    progs

---
+++ sum_average.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,sum=0;
+    float average;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    average =(a+b+c)/3;
+    printf("average = %f\n",average);
+    printf("sum=%d",a+b+c);
+}
