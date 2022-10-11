51c2b601790c002e99f1b371c89134a15704b7ad
Rishabh Kumar Sharma <rishabhsharma28302@gmail.com>
2022-10-11 12:47:00 +0530

    initial commit

--- Linked_List.c
+++ Linked_List.c
@@ -11,11 +11,51 @@
    struct node *Head=NULL;
    struct node *Tail  = NULL;
 void insertFirst(int key) {
-   struct node *link = (struct node*) malloc(sizeof(struct node));
-   link->val = key;
-   link->next = Head;
-   Head = link;
+   struct node *newnode=(struct node *)malloc(sizeof(struct node));
+   newnode->next=Head;
+   newnode->val=key;
+   Head=newnode;
 }
+// void insertlast(int key) {
+//    struct node *newnode = (struct node*) malloc(sizeof(struct node));
+//    struct node *ptr;
+//    ptr=Head;
+//    while(ptr!=NULL){
+//       ptr=ptr->next;
+//    }
+//    ptr->next=newnode;
+//    newnode->next=NULL;
+//    newnode->val = key;
+// }
+void printList() {
+   struct node *ptr = Head;
+   while(ptr != NULL) {
+      printf("%d ->",ptr->val);
+      ptr = ptr->next;
+   }
+}
+void display(){
+    struct node *ptr;
+    ptr=Head;
+    while (ptr != NULL)
+    { 
+        printf("%d->",ptr->val);
+        ptr=ptr->next;
+    }
+    printf("END");
+ 
+}
+void customPrint(int key){
+   struct node *ptr;
+   ptr=Head;
+   while(key-1>0){
+     ptr=ptr->next;
+     key--;
+   }
+   printf("%d ",ptr->val);
+
+}
+
 int main(){ 
    insertFirst(1);
    insertFirst(2);
@@ -23,13 +63,5 @@ int main(){
    insertFirst(4);
    insertFirst(5);
    insertFirst(6);
-   printList();
-
-}
-void printList() {
-   struct node *ptr = Head;
-   while(ptr != NULL) {
-      printf("%d ->",ptr->val);
-      ptr = ptr->next;
-   }
+   customPrint(3);
 }
